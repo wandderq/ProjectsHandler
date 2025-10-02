732513fadc763b7970095f4eb13787ada51fd6f9
wandderq <wanddr.qq@gmail.com>
2025-10-02 19:22:46 +0300

    Code cleanup

--- src/core/structures.py
+++ src/core/structures.py
@@ -1,26 +1,27 @@
 class Project:
-    def __init__(self,
-            name: str,
-            path: str,
-            version: str,
-            languages: list,
-            frameworks: list,
-            description: str
-        ) -> None:
-        
+    def __init__(
+        self,
+        name: str,
+        path: str,
+        version: str,
+        languages: list,
+        frameworks: list,
+        description: str,
+    ) -> None:
+
         self.name = name
         self.path = path
         self.version = version
         self.languages = languages
         self.frameworks = frameworks
         self.description = description
-    
+
     def json(self) -> dict:
         return {
-            'name': self.name,
-            'path': self.path,
-            'version': self.version,
-            'language': self.languages,
-            'framevorks': self.frameworks,
-            'description': self.description
+            "name": self.name,
+            "path": self.path,
+            "version": self.version,
+            "language": self.languages,
+            "framevorks": self.frameworks,
+            "description": self.description,
         }
--- src/core/tui.py
+++ src/core/tui.py
@@ -1,14 +1,13 @@
-import os
-
 from argparse import Namespace
 from core.handler import ProjectsHandler
 from core.structures import Project
+
 
 class TerminalUserInterface:
     def __init__(self, args: Namespace) -> None:
         self.handler = ProjectsHandler(args.projects_path)
         self.args = args
-    
+
     def init_command(self) -> None:
         self.handler.init_project_files(
             Project(
@@ -17,15 +16,12 @@ class TerminalUserInterface:
                 version=self.args.version,
                 languages=self.args.languages,
                 frameworks=self.args.frameworks,
-                description=self.args.description
+                description=self.args.description,
             )
         )
-    
-    def info_command(self) -> None:
-        ...
-    
-    def list_command(self) -> None:
-        ...
-    
-    def update_command(self) -> None:
-        ...
+
+    def info_command(self) -> None: ...
+
+    def list_command(self) -> None: ...
+
+    def update_command(self) -> None: ...
